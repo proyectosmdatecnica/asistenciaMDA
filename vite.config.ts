fee3add5ca2575774700191bb00cac527be25b0d
proyectosmdatecnica <proyectosmdatecnica@gmail.com>
2026-01-12 12:48:07 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,6 +5,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    sourcemap: false
   },
+  server: {
+    port: 3000
+  }
 });
